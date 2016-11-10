0fe9dc6904daf87268bdbdee65b28c9c5387b86f
ShadowsOftheBeast <magnuski@hotmail.com>
2016-11-10 16:14:18 +0000

    Created GenderPayGap empty application scaffolding.

---
+++ Alpha/GenderPayGap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GenderPayGap.Startup))]
+namespace GenderPayGap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
