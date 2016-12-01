f4cf160692bafe383cfab2404382446a609d3d85
ShadowsOftheBeast <magnuski@hotmail.com>
2016-12-01 11:35:35 +0000

    test ci trigger 1

--- Alpha/GenderPayGap.Tests/Controllers/HomeControllerTest.cs
+++ Alpha/GenderPayGap.Tests/Controllers/HomeControllerTest.cs
@@ -16,11 +16,12 @@ namespace GenderPayGap.Tests.Controllers
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController();  
+            HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult; 
-             
+            ViewResult result = controller.Index() as ViewResult;
+
+            int a = 1;
             // Assert
             //Positive Test:
            //Assert.IsNotNull(result);
