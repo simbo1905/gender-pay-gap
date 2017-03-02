d5b9e6d85e487973f99e27c7e632adaf450ba1cb
stephen.mccabe <stephen.mccabe@cadencepartnership.com>
2017-03-02 15:21:52 +0000

    Added mocking for PublicSectorRepository and PRivateSectorRepository

--- Beta/GenderPayGap.Core/Interfaces/IPagedRepository.cs
+++ Beta/GenderPayGap.Core/Interfaces/IPagedRepository.cs
@@ -4,6 +4,9 @@ namespace GenderPayGap.Core.Interfaces
 {
     public interface IPagedRepository<T>
     {
+        void Insert(T entity);
+        void Delete(T entity);
+
         PagedResult<T> Search(string searchText, int page, int pageSize);
     }
 }
