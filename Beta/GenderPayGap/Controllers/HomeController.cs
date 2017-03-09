f2ca14a54d37fd7b6fd8afd348dc3b1ddbab7627
stephen.mccabe <stephen.mccabe@cadencepartnership.com>
2017-03-09 14:25:58 +0000

    Got viewing service working

--- Beta/GenderPayGap/Controllers/HomeController.cs
+++ Beta/GenderPayGap/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using GenderPayGap.Models.SqlDatabase;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using GenderPayGap.Models.SqlDatabase;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.Security;
 using Autofac;
+using Extensions;
+using GenderPayGap.Core.Classes;
 using GenderPayGap.WebUI.Classes;
+using GenderPayGap.WebUI.Properties;
 
 namespace GenderPayGap.WebUI.Controllers
 {
@@ -72,8 +80,20 @@ namespace GenderPayGap.WebUI.Controllers
                     Session.Abandon();
                     Request.GetOwinContext().Authentication.SignOut();
                     break;
+                case "CreateTestData":
+                    CreateTestData();
+                    break;
                 case "ClearDatabase":
                     DbContext.Truncate();
+
+                    var downloadsLocation = FileSystem.ExpandLocalPath(Settings.Default.DownloadsLocation);
+                    if (Directory.Exists((downloadsLocation)))
+                        foreach (var file in Directory.GetFiles(downloadsLocation))
+                            System.IO.File.Delete(file);
+
+                    //Refresh the repository
+                    DataRepository = null;
+
                     if (User.Identity.IsAuthenticated)
                     {
                         Session.Abandon();
@@ -82,6 +102,150 @@ namespace GenderPayGap.WebUI.Controllers
                     break;
             }
             return RedirectToAction("Index");
+        }
+
+        void CreateTestData(int recordCount=500, int yearCount=3)
+        {
+            var organisations = DataRepository.GetAll<Organisation>();
+            var sicCodes = DataRepository.GetAll<SicCode>();
+            while (organisations.Count() < recordCount)
+            {
+                var sector = Numeric.Rand(0, 1) == 0 ? SectorTypes.Private : SectorTypes.Public;
+                string searchText = Text.AlphabetChars[Numeric.Rand(0, 1)].ToString();
+                var organisation=new Organisation();
+                PagedResult<EmployerRecord> result;
+                if (sector == SectorTypes.Public)
+                {
+                    try
+                    {
+                        result = PublicSectorRepository.Search(searchText, Numeric.Rand(1, recordCount), 10);
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                    if (result == null || result.RowCount == 0 || result.Results == null || result.Results.Count == 0) continue;
+                    foreach (var employer in result.Results)
+                    {
+                        if (organisations.Any(o => o.OrganisationName==employer.Name)) continue;
+                        organisation=new Organisation();
+                        organisation.OrganisationName = employer.Name;
+                        organisation.SectorType = sector;
+                        organisation.Status= OrganisationStatuses.Active;
+                        organisation.StatusDetails = "TEST DATA";
+                        DataRepository.Insert(organisation);
+                        DataRepository.SaveChanges();
+
+                        DataRepository.Insert(new OrganisationAddress()
+                        {
+                            OrganisationId = organisation.OrganisationId,
+                            Address1 = "Address line " + organisation.OrganisationId,
+                            Address3 = "City" + organisation.OrganisationId,
+                            Country = "Country" + organisation.OrganisationId,
+                            PostCode = "Post Code" + organisation.OrganisationId,
+                            Status = AddressStatuses.Active
+                        });
+
+                        DataRepository.Insert(new OrganisationSicCode()
+                        {
+                            OrganisationId = organisation.OrganisationId,
+                            SicCodeId = 1
+                        });
+                    }
+                }
+                else if (sector == SectorTypes.Private)
+                {
+                    try
+                    {
+                        result = PrivateSectorRepository.Search(searchText, Numeric.Rand(1, recordCount), 10);
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                    if (result == null || result.RowCount == 0 || result.Results == null || result.Results.Count == 0) continue;
+                    foreach (var employer in result.Results)
+                    {
+                        if (organisations.Any(o => o.OrganisationName==employer.Name)) continue;
+                        organisation = new Organisation();
+                        organisation.OrganisationName = employer.Name;
+                        organisation.SectorType = sector;
+                        organisation.Status = OrganisationStatuses.Active;
+                        organisation.StatusDetails = "TEST DATA";
+                        organisation.PrivateSectorReference = employer.CompanyNumber;
+                        DataRepository.Insert(organisation);
+                        DataRepository.SaveChanges();
+
+                        DataRepository.Insert(new OrganisationAddress()
+                        {
+                            OrganisationId = organisation.OrganisationId,
+                            Address1 = employer.Address1,
+                            Address2 = employer.Address2,
+                            Address3 = employer.Address3,
+                            Country = employer.Country,
+                            PostCode = employer.PostCode,
+                            PoBox = employer.PoBox,
+                            Status = AddressStatuses.Active
+                        });
+
+                        employer.SicCodes = PrivateSectorRepository.GetSicCodes(employer.CompanyNumber);
+
+                        foreach (var sicCode in employer.SicCodes.SplitI())
+                        {
+                            var code = sicCode.ToInt32();
+                            if (!sicCodes.Any(s => s.SicCodeId == code))
+                            {
+                                MvcApplication.Log.WriteLine($"Invalid SIC code '{code}' received from companies house");
+                                continue;
+                            }
+                            if (organisation.OrganisationSicCodes.Any(a => a.SicCodeId == code)) continue;
+                            DataRepository.Insert(new OrganisationSicCode()
+                            {
+                                OrganisationId = organisation.OrganisationId,
+                                SicCodeId = code
+                            });
+                        }
+                        DataRepository.SaveChanges();
+                    }
+                }
+            }
+
+            foreach (var organisation in organisations.ToList())
+            {
+                for (var year = DateTime.Now.Year; year >= (DateTime.Now.Year - yearCount); year--)
+                {
+                    var returns = organisation.Returns.ToList();
+                    var accountingDate = GetAccountYearStartDate(organisation.SectorType, year);
+                    var @return = returns.FirstOrDefault(r => r.AccountingDate == accountingDate);
+                    if (@return != null) continue;
+                    DataRepository.Insert(new Return()
+                    {
+                        OrganisationId = organisation.OrganisationId,
+                        AccountingDate = accountingDate,
+                        Status = ReturnStatuses.Submitted,
+                        StatusDate = accountingDate.AddDays(Numeric.Rand(0, 365)),
+                        CompanyLinkToGPGInfo = "https://" + organisation.OrganisationName.ReplaceI(" ", "") + ".co.uk/GPGData",
+                        DiffMeanBonusPercent = Numeric.Rand(0, 100),
+                        DiffMeanHourlyPayPercent = Numeric.Rand(0, 100),
+                        DiffMedianBonusPercent = Numeric.Rand(0, 100),
+                        DiffMedianHourlyPercent = Numeric.Rand(0, 100),
+                        FemaleLowerPayBand = Numeric.Rand(0, 100),
+                        FemaleMedianBonusPayPercent = Numeric.Rand(0, 100),
+                        FemaleMiddlePayBand = Numeric.Rand(0, 100),
+                        FemaleUpperPayBand = Numeric.Rand(0, 100),
+                        FemaleUpperQuartilePayBand = Numeric.Rand(0, 100),
+                        FirstName = "Firstname" + organisation.OrganisationId,
+                        LastName = "Lastname" + organisation.OrganisationId,
+                        JobTitle = "Jobtitle " + organisation.OrganisationId,
+                        MaleLowerPayBand = Numeric.Rand(0, 100),
+                        MaleMedianBonusPayPercent = Numeric.Rand(0, 100),
+                        MaleUpperQuartilePayBand = Numeric.Rand(0, 100),
+                        MaleMiddlePayBand = Numeric.Rand(0, 100),
+                        MaleUpperPayBand = Numeric.Rand(0, 100)
+                    });
+                }
+            }
+            DataRepository.SaveChanges();
         }
 
         [Route("SignOut")]
